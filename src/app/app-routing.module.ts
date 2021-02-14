60c4fe3d9b401fb0577bcdb33b4e7c7b67b23874
Jishnu <jishnu@waitwhile.com>
2021-02-14 12:13:53 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./containers/login/login.module").then(m => m.LoginModule),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
